b89b67ef0c429a07433caec5ffbe2340ecd374b4
kavyantv1-ship-it <kavyantv1@gmail.com>
2026-09-08 18:08:42 +0500

    Replace SAMEER2DAY key with GRW_4.6 2DAY KEY

--- keylist.lua
+++ keylist.lua
@@ -17,7 +17,7 @@ return {
         max_devices = 5,
         SLOT = "2"
     }
-    ["SAMEER2DAY"] = {
+    ["GRW_4.6 2DAY KEY"] = {
         type = "DEMO",
         expiry = "2026-09-10",
         valid = true,
