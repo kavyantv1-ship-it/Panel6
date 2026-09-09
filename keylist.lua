f616567c2ceb543ee8f795d982ffee6370aa42bc
kavyantv1-ship-it <kavyantv1@gmail.com>
2026-09-09 18:20:34 +0500

    Increase max_devices from 3 to 4 for SAMEER

--- keylist.lua
+++ keylist.lua
@@ -14,7 +14,7 @@ return {
         type = "DEMO",
         expiry = "2026-10-25",
         valid = true,
-        max_devices = 3,
+        max_devices = 4,
         SLOT = "2"
     }
     ["GRW"] = {
